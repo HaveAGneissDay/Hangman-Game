e2e5278433ee1c51384e28fb9176e52ad116c3db
David Cheng <dkcheng10@gmail.com>
2017-11-13 13:20:09 -0800

    intial setup

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+//HangMan Game 
+
+// Counter for wins and loses
+
+// An array of words to choose from
+
+//function to display the number of guesses counting down
+
+//function to display the letters guessed
+
+// function to replace each letter with a blank at the start of the game
+
+// function to replace each blank with a letter when guessed
+
+// function to choose a new word after the word is guessed or not guessed
